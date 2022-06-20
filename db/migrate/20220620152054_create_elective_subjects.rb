f3528f3386fa5b85633f5e35648c57dcad084160
Lam Nguyen <sogoten6689@gmail.com>
2022-06-20 17:27:50 +0700

    - migrate elective subject table

---
+++ db/migrate/20220620152054_create_elective_subjects.rb
@@ -0,0 +1,13 @@
+class CreateElectiveSubjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :elective_subjects do |t|
+      t.string :group_subject
+      t.string :thematic_group
+      t.string :elective_subject_one
+      t.string :elective_subject_two
+      t.string :alternative_subject
+      t.boolean :editable, default: true
+      t.timestamps
+    end
+  end
+end
